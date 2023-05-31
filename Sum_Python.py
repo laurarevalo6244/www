a7f8f86d0051c55e9dd32ebd7cadfc698009282a
laurarevalo6244 <135129078+laurarevalo6244@users.noreply.github.com>
2023-05-31 07:50:30 -0500

    Update Sum_Python.py

--- Sum_Python.py
+++ Sum_Python.py
@@ -1,1 +1,13 @@
+frutas = ["manzana", "banana", "naranja"]
+print(frutas[0])  # Imprime "manzana"
 
+frutas.append("pera")
+print(frutas)  # Imprime ["manzana", "banana", "naranja", "pera"]
+
+for fruta in frutas:
+    print(fruta)
+
+#LAURA_AREVALO
+#FRUTAS
+#IEM_ESCUELA_NORMAL_PASTO
+#111
